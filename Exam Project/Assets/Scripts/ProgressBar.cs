b2f003b930af511b8939c2d1e349a8c73b8eb84b
Amro <2592997@students.wits.ac.za>
2023-10-24 00:48:14 +0200

    Finished Taskbar

--- Exam Project/Assets/Scripts/ProgressBar.cs
+++ Exam Project/Assets/Scripts/ProgressBar.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProgressBar : MonoBehaviour
 {
+    public TextMeshProUGUI TaskMessage;
+    public GameObject Parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,11 @@ public class ProgressBar : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddTask(string Task)
+    {
+        Instantiate(TaskMessage, Parent.transform);
+        TaskMessage.text = Task;
     }
 }
