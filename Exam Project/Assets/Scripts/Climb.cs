1c5093ea18184cde792b288ae066063ee6bda8c5
Amro <2592997@students.wits.ac.za>
2023-11-03 23:43:46 +0200

    Added 4th Ability

---
+++ Exam Project/Assets/Scripts/Climb.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climb : MonoBehaviour
+{
+    public PlayerController playerController;
+    public bool Climbing = false;
+    public float moveSpeed;
+    public Rigidbody PlayerRb;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Climbing)
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(0.0f, verticalInput, 0.0f) * moveSpeed * Time.deltaTime;
+            transform.Translate(movement);
+            PlayerRb.useGravity = false;
+            PlayerRb.mass = 0f;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Climbable"))
+        {
+            Climbing = true;
+            playerController.enabled = false;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Climbable"))
+        {
+            Climbing = false;
+            playerController.enabled = true;
+            PlayerRb.useGravity = true;
+            PlayerRb.mass = 1f;
+        }
+    }
+}
