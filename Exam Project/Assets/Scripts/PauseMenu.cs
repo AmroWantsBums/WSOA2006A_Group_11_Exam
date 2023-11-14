e8aaa1227b9a7b8f30350c65d09985f548fbfdac
DanzoLuvsKFC <131270351+DanzoLuvsKFC@users.noreply.github.com>
2023-11-14 13:45:10 +0200

    Added the pause menu

---
+++ Exam Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public PlayerController playercontroller;
+    public GameObject pauseMenuUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 0f)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+
+        // Enable the pause menu UI
+        pauseMenuUI.SetActive(true);
+        playercontroller.enabled = false;
+    }
+
+    void ResumeGame()
+    {
+        // Unfreeze time
+        Time.timeScale = 1f;
+
+        // Disable the pause menu UI
+        pauseMenuUI.SetActive(false);
+        playercontroller.enabled = true;
+    }
+}
